a0e1d4c51e1769ce7fd6da7820989ef524cb6ecd
yannis <yannistarfa98@gmail.com>
2024-10-25 18:46:14 -0400

    InterviewManagerState ABC, add abstract method decorators, add get_init_message method and __change_state dependency

--- src/core/interview/interview_manager_state.py
+++ src/core/interview/interview_manager_state.py
@@ -1,12 +1,23 @@
+import abc
 from abc import ABC
+from collections.abc import Callable
 from typing import Optional
+from typing_extensions import Self
 
+from ...domain.models.message import Message
 from ...domain.models.session import Session
 
 
 class InterviewManagerState(ABC):
-    def __init__(self, session: Session):
+    def __init__(self, change_state: Callable[Self, None], session: Session):
+        self.__change_state = change_state
         self.session = session
 
-    async def handle_message(self, message: Optional[str]):
+    @abc.abstractmethod
+    async def handle_message(self, message: Optional[str]) -> Message:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_init_message() -> Optional[Message]:
         pass
