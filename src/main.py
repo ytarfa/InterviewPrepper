ffb07a147b9f80c62a2b8bd0977cde42953d2c2a
yannis <yannistarfa98@gmail.com>
2024-10-25 18:33:56 -0400

    Add .env to gitignore, load .env in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from .api.sessions_routes import sessions_router
 
+load_dotenv()
 app = FastAPI(title="Interview Prepper API")
 app.include_router(sessions_router)
