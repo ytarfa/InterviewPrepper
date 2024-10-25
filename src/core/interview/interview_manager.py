a61fe23baec283513a388974eaf190b4cb078159
yannis <yannistarfa98@gmail.com>
2024-10-25 18:47:25 -0400

    InterviewManager: Add Resume and JobDescription states, write user message and response to session object, pass correct dependencies to state

--- src/core/interview/interview_manager.py
+++ src/core/interview/interview_manager.py
@@ -1,12 +1,17 @@
 from collections.abc import Callable
 
 from .interview_manager_state import InterviewManagerState
+from .job_description_state import InterviewManagerJobDescriptionState
+from .resume_state import InterviewManagerResumeState
 from ..session.session_service import SessionService
 from .start_state import InterviewManagerStartState
+from ...domain.models.message import Message, MessageType
 from ...domain.models.session import SessionStage
 
 step_map: dict[SessionStage, Callable[..., InterviewManagerState]] = {
     SessionStage.START: InterviewManagerStartState,
+    SessionStage.RESUME: InterviewManagerResumeState,
+    SessionStage.JOB_DESCRIPTION: InterviewManagerJobDescriptionState
 }
 
 
@@ -19,15 +24,25 @@ class InterviewManager:
 
     async def initialize(self):
         session = self.session_service.get_session(self.session_id)
-        print(session)
-        state = step_map[session["stage"]](session)
+        state = step_map[session.stage](change_state=self.change_state, session=session)
         self.change_state(state)
 
     def change_state(self, state: InterviewManagerState):
         self.state = state
 
-    async def handle_message(self, message: str):
+    async def handle_message(self, message: str) -> Message:
         if not self.ready:
             await self.initialize()
-        response: str = await self.state.handle_message(message=message)
-        print(response)
+
+        # Write user message to session messages
+        self.session_service.add_messages(session_id=self.session_id, messages=[
+            Message(
+                content=message,
+                type=MessageType.USER
+            )
+        ])
+        # Handle message
+        response = await self.state.handle_message(message=message)
+        # Write response to session messages
+        self.session_service.add_messages(session_id=self.session_id, messages=[response])
+        return response
