6b2c6a69dbeece69b85bf54f4b55f6e16e1e0d46
yannis <yannistarfa98@gmail.com>
2024-10-25 18:45:33 -0400

    Create llm.py

---
+++ src/infrastructure/llm.py
@@ -0,0 +1,8 @@
+import os
+
+from langchain_anthropic import ChatAnthropic
+
+def claude_haiku():
+    api_key = os.environ.get("ANTHROPIC_API_KEY")
+    if api_key is not None:
+        return ChatAnthropic(model="claude-3-haiku-20240307", api_key=api_key)
