156d19de933099157006b399c7812a811ff4a054
yannis <yannistarfa98@gmail.com>
2024-10-25 15:55:02 -0400

    Setup InterviewManager initialization

--- src/core/interview/interview_manager.py
+++ src/core/interview/interview_manager.py
@@ -1,20 +1,33 @@
+from collections.abc import Callable
+
+from .interview_manager_state import InterviewManagerState
 from ..session.session_service import SessionService
+from .start_state import InterviewManagerStartState
+from ...domain.models.session import SessionStage
 
-from abc import ABC
-
-
-class InterviewManagerState(ABC):
-    def __init__(self, session_service: SessionService, session_id: str):
-        self.session_service = session_service
-        self.session = self.session_service.get_session(session_id)
-
-    def handle_message(self):
-        pass
+step_map: dict[SessionStage, Callable[..., InterviewManagerState]] = {
+    SessionStage.START: InterviewManagerStartState,
+}
 
 
 class InterviewManager:
-    def __init__(self, state: InterviewManagerState):
+    def __init__(self, session_id: str, session_service: SessionService):
+        self.ready = False
+        self.session_id = session_id
+        self.session_service = session_service
+        self.state = None
+
+    async def initialize(self):
+        session = self.session_service.get_session(self.session_id)
+        print(session)
+        state = step_map[session["stage"]](session)
+        self.change_state(state)
+
+    def change_state(self, state: InterviewManagerState):
         self.state = state
 
-    def handle_message(self):
-        self.state.handle_message()
+    async def handle_message(self, message: str):
+        if not self.ready:
+            await self.initialize()
+        response: str = await self.state.handle_message(message=message)
+        print(response)
