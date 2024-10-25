739f3f2f5d9c59524b2599f214d02777fcc640d2
yannis <yannistarfa98@gmail.com>
2024-10-25 15:58:14 -0400

    Change handle_message argument type in InterviewManagerStartState

--- src/core/interview/start_state.py
+++ src/core/interview/start_state.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from .interview_manager_state import InterviewManagerState
 
 
@@ -5,5 +7,5 @@ class InterviewManagerStartState(InterviewManagerState):
     def __init__(self, session):
         super().__init__(session)
 
-    async def handle_message(self, message: str):
+    async def handle_message(self, message: Optional[str]):
         return "Welcome to the interview manager. Type 'start' to begin a new interview session."
