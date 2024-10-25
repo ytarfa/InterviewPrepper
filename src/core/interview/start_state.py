ce1c8b2b93408417f40dda001f3e24a27a3c5e7a
yannis <yannistarfa98@gmail.com>
2024-10-25 15:52:12 -0400

    Created dummy InterviewManagerStartState

---
+++ src/core/interview/start_state.py
@@ -0,0 +1,9 @@
+from .interview_manager_state import InterviewManagerState
+
+
+class InterviewManagerStartState(InterviewManagerState):
+    def __init__(self, session):
+        super().__init__(session)
+
+    async def handle_message(self, message: str):
+        return "Welcome to the interview manager. Type 'start' to begin a new interview session."
